d0161fa237956162d5017edb1be0616b6ab4334f
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2017-01-25 17:27:15 +0100

    Added a Header.js file
    It doesn't work :-1:

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = ({breadcrumb}) => {
+  return (
+    <h1>Ruta:{breadcrumb}</h1>
+  );
+}
+
+export default Header;
--- src/views/App.js
+++ src/views/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 import Navbar from './../components/Navbar';
+import Header from './../components/Header';
 
 import './App.css';
 
 import logo from './../assets/logo.svg';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentBreadcrumb: this.props.location.pathname
+    }
+  }
+
+  componentDidMount() {
+    this.setState({currentBreadcrumb: this.props.location.pathname})
+  }
+
   render() {
     return (
       <div className="App">
+        <Header breadcrumb={this.state.currentBreadcrumb}/>
         <Navbar logo={logo} />
         <main className="App__content">
           {this.props.children}
