f1f5b6cd143c5311c91233d660e94d596e0e21fe
César Alberca <cesalberca@gmail.com>
2017-01-27 18:59:49 +0100

    Returns data from API

--- src/components/CinemasList.js
+++ src/components/CinemasList.js
@@ -8,17 +8,33 @@ class CinemasList extends React.Component {
 
     this.state = {
       cinemas: []
-    }
+    };
   }
 
   componentDidMount() {
-
+    fetch('http://localhost:8080/api/cinemas', {
+      method: 'GET'
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json)
+      this.setState({cinemas: json._embedded.cinemas});
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   render () {
+    const cinemasList = this.state.cinemas.map((cinema) => {
+      return <li key={cinema._links.self.href}>Cinema: {cinema.nombre}</li>
+    });
+
     return (
-        
-    )
+        <ul>{cinemasList}</ul>
+    );
   }
 }
 
