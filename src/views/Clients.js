609a7405570fbe768a906db3aa243da89d5e22e3
César Alberca <cesalberca@gmail.com>
2017-02-13 18:58:15 +0100

    Modify Clients.js

--- src/views/Clients.js
+++ src/views/Clients.js
@@ -1,16 +1,78 @@
 import React from 'react';
 
-import ClientsList from './../components/ClientsList';
+import TableComponent from './../components/TableComponent';
 import FormContainer from './../components/FormContainer';
+import ModalForm from './../components/ModalForm';
 import FormActions from './../components/FormActions';
+import { selectAll } from './../utils/apiHelper';
 
 class Clients extends React.Component {
-  render() {
-    return(
+    constructor(props) {
+    super(props);
+
+    this.state = {
+      clients: []
+    };
+
+this.clientsColumn = [
+      {
+        name: 'ID',
+        field: 'idCliente'
+      },
+      {
+        name: 'Nombre',
+        field: 'nombre'
+      },
+      {
+        name: 'Apellidos',
+        field: 'apellidos'
+      },
+      {
+        name: 'DNI',
+        field: 'dni'
+      },
+      {
+        name: 'E-mail',
+        field: 'email'
+      }
+    ];
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.refresh = this.refresh.bind(this);
+  }
+
+  componentDidMount() {
+    selectAll('clients')
+    .then((response) => {
+      this.setState({cinemas: response._embedded.cinemas})
+    });
+  }
+
+  handleOpenModal() {
+    this.setState({isOpen: true});
+  }
+
+  handleCloseModal() {
+    this.setState({isOpen: false});
+  }
+
+  refresh() {
+
+  }
+
+render() {
+    return (
       <div>
         <h1>Clientes</h1>
-        <FormContainer targetTable="clients" title="New client" actionButtonText="Create new client"/>
-        <FormActions/>
+
+        <FormActions handleOpenModal={this.handleOpenModal} refresh={this.refresh}/>
+
+        <TableComponent data={this.state.clients} columns={this.clientsColumn} />
+
+        <ModalForm modalTitle="Modal cines" isOpen={this.state.isOpen} handleCloseModal={this.handleCloseModal}>
+          <FormContainer targetTable="p" title="Nueva pelicula" actionButtonText="Crear nueva pelicula"/>
+        </ModalForm>
       </div>
     );
   }
