76781ea5a9a93853af4685beeacfe8a647b75495
César Alberca <cesalberca@gmail.com>
2017-02-14 20:42:36 +0100

    Format and cleanup

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -9,16 +9,14 @@ class FormContainer extends React.Component{
     super(props);
 
     this.state = {
-      title: this.props.title,
       tableColumns: [],
       formItems: [],
-      formItemsValues: {},
-      actionButtonText: this.props.actionButtonText
+      formItemsValues: {}
     };
 
     this.loadFormItems = this.loadFormItems.bind(this);
     this.insertNewEntry = this.insertNewEntry.bind(this);
-    this.formItemsOnChange = this.formItemsOnChange.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
   }
 
   componentDidMount() {
@@ -34,13 +32,13 @@ class FormContainer extends React.Component{
     })
     .then((json) => {
       const tableColumns = json.alps.descriptors[0].descriptors;
-      this.setState({tableColumns})
+      this.setState({tableColumns});
     })
     .catch((error) => {
     });
   }
 
-  formItemsOnChange(e) {
+  handleOnChange(e) {
     let changedValues = {};
     changedValues[e.target.name] = e.target.value;
     const updatedItems = Object.assign(this.state.formItemsValues, changedValues);
@@ -48,29 +46,30 @@ class FormContainer extends React.Component{
   }
 
   insertNewEntry() {
-    const body = this.state.formItemsValues;
-
-    insert(this.props.targetTable, body)
+    insert(this.props.targetTable, this.state.formItemsValues)
     .then(() => {
-      swal({title: 'Éxito', type: 'success'});
+      swal({title: 'Entrada creada', type: 'success'});
     })
-    .catch(error => console.log(error));
+    .catch(error => {
+      swal({title: 'Error al crear entrada', type: 'error'});
+    });
   }
 
   render() {
     const formItemsList = this.state.tableColumns.map((column, index) => {
       return(
-        <FormItem label={column.name} onChangeEvent={this.formItemsOnChange} key={index} inputName={column.name} inputType="text"/>
+        <FormItem label={column.name} onChangeEvent={this.handleOnChange} key={index} inputName={column.name} inputType="text"/>
       );
-    })
+    });
 
+    const { title, actionButtonText } = this.props;
     return (
       <div className="FormContainer">
-        <h1 className="FormContainer__Title">{this.state.title}</h1>
+        <h1 className="FormContainer__Title">{title}</h1>
         <form className="FormContainer__Form">
           {formItemsList}
           <div className="FormContainer__container__actionButton">
-            <input onClick={this.insertNewEntry} className="FormContainer__actionButton btn btn--primary" type="button" value={this.state.actionButtonText} />
+            <input onClick={this.insertNewEntry} className="FormContainer__actionButton btn btn--primary" type="button" value={actionButtonText}/>
           </div>
         </form>
       </div>
