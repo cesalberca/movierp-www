f2fdb7a419153905bbec5670d15e3ddb5e88cd1c
César Alberca <cesalberca@gmail.com>
2017-02-18 19:23:25 +0100

    Views are dynamically generated
    Created ViewGenerator component which recieves all the needed data for a view to be created.

--- src/views/Orders.js
+++ src/views/Orders.js
@@ -1,41 +1,26 @@
 import React from 'react';
 
-import OrdersList from './../components/OrdersList';
-import FormContainer from './../components/FormContainer';
-import ModalForm from './../components/ModalForm';
+import ViewGenerator from './ViewGenerator';
 
 class Orders extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      isOpen: false
-    }
-
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
-  }
-
-  handleOpenModal(e) {
-    this.setState({isOpen: true})
-  }
-
-  handleCloseModal() {
-      this.setState({isOpen: false});
+    this.columns = [
+      {
+        name: 'ID',
+        field: 'id'
+      },
+      {
+        name: 'ID Proveedor',
+        field: 'idProveedor'
+      }
+    ];
   }
 
   render() {
     return (
-      <div>
-        <h1>Orders</h1>
-        <OrdersList />
-
-        <ModalForm isOpen={this.state.isOpen} handleCloseModal={this.handleCloseModal}>
-          <FormContainer targetTable="orders" title="New order" actionButtonText="Create new order"/>
-        </ModalForm>
-
-        <button onClick={this.handleOpenModal}>Abrir modal</button>
-      </div>
+      <ViewGenerator title="Pedidos" table="orders" columns={this.columns}/>
     );
   }
 }
