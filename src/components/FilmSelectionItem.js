c909b73abac985b90d18571bfc9e80fe5d34a985
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2017-02-27 17:41:50 +0100

    Fixed typo in FilmSelectionItem

--- src/components/FilmSelectionItem.js
+++ src/components/FilmSelectionItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 const FilmSelectionItem = ({poster, title, duration, synopsis, filmId, cinemaId}) => {
   return(
-    <Link to={`/tickets/${filmId}/${cinemaId}`} className="card">
+    <Link to={`/tickets/${cinemaId}/${filmId}`} className="card">
       <h1 className="card__title">{title}</h1>
       <h3 className="card__subtitle">Duración - {duration}</h3>
       <p className="card__text">{synopsis}</p>
